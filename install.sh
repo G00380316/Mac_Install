ce99217bfd9095e5667191371876799aad806888
Enoch Abiodun <117421820+G00380316@users.noreply.github.com>
2026-01-23 00:26:29 +0000

    Need treesitter package

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ packages=(
 	"temurin@8" "temurin@21" "ripgrep" "libplist" "ipatool"
 	"font-jetbrains-mono" "font-caskaydia-cove-nerd-font" "watchman" "ngrok"
 	"db-browser-for-sqlite" "fd" "bat" "github" "tldr" "git-lfs" "hammerspoon"
-	"mactex-no-gui"
+	"mactex-no-gui" "tree-sitter-cli"
 
 	"lua-language-server" "basedpyright" "typescript-language-server" "xcode-build-server"
 	"bash-language-server" "texlab" "harper" "jdtls" "markdown-oxide"
