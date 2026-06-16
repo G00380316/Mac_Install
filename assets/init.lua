603d6520caeb958314d2e602d679401a4ab17ccd
Enoch Abiodun <117421820+G00380316@users.noreply.github.com>
2026-06-16 01:25:40 +0100

    New OS Navigation

--- assets/init.lua
+++ assets/init.lua
@@ -17,19 +17,6 @@ hs.window.filter.default:subscribe(hs.window.filter.windowFullscreened, function
     end
 end)
 
--- =========================
--- Keymaps
--- =========================
-
--- Full Screen
-hs.hotkey.bind({ "alt", "shift" }, "f", function()
-    local win = hs.window.focusedWindow()
-    if not win then return end
-
-    win:setFullscreen(not win:isFullscreen())
-end)
-
-
 local function launchApp(appName, opts)
     opts = opts or {}
     local fullscreen = opts.fullscreen
@@ -64,83 +51,6 @@ local function launchApp(appName, opts)
     end)
 end
 
--- Droppy
-
-local activeKey = nil
-local oldPos = nil
-
-local function getNotchPos()
-    local frame = hs.screen.mainScreen():fullFrame()
-    return {
-        x = frame.x + frame.w / 2,
-        y = frame.y + 25,
-    }
-end
-
-local function isInNotchZone(pos, notchPos)
-    return math.abs(pos.x - notchPos.x) <= 200
-        and math.abs(pos.y - notchPos.y) <= 75
-end
-
-local function focusDroppy()
-    local app = hs.application.get("Droppy")
-    if app then
-        app:activate(true)
-    end
-end
-
-local function activateDroppy(key)
-    local notchPos = getNotchPos()
-    local currentPos = hs.mouse.absolutePosition()
-
-    if not isInNotchZone(currentPos, notchPos) then
-        oldPos = currentPos
-    end
-
-    hs.mouse.absolutePosition(notchPos)
-    focusDroppy()
-
-    activeKey = key
-end
-
-local function transitionDroppy(key)
-    if oldPos then
-        hs.mouse.absolutePosition(oldPos)
-    end
-
-    hs.timer.doAfter(0.01, function()
-        activateDroppy(key)
-    end)
-end
-
-local function deactivateDroppy()
-    if oldPos then
-        hs.mouse.absolutePosition(oldPos)
-    end
-
-    activeKey = nil
-end
-
-local keys = { "D", "W", "M", "N", "space", "A", "T" }
-
-for _, key in ipairs(keys) do
-    hs.hotkey.bind({ "alt", "shift" }, key, function()
-        if activeKey == key then
-            deactivateDroppy()
-        elseif activeKey ~= nil then
-            transitionDroppy(key)
-        else
-            activateDroppy(key)
-        end
-    end)
-end
-
--- local app = hs.application.get("Droppy")
--- print(hs.inspect(app:allWindows()))
--- for _, app in ipairs(hs.application.runningApplications()) do
---     print(app:name())
--- end
---
 --------------------------------------------------
 -- Kitty shortcuts
 --------------------------------------------------
@@ -166,30 +76,32 @@ local function launchKittyWithNvim()
         :start()
 end
 
-hs.hotkey.bind({ "cmd", "shift" }, "return", function()
-    launchKittyWithNvim()
+
+-- =========================
+-- Keymaps
+-- =========================
+
+-- Full Screen
+hs.hotkey.bind({ "alt", "shift" }, "f", function()
+    local win = hs.window.focusedWindow()
+    if not win then return end
+
+    win:setFullscreen(not win:isFullscreen())
 end)
 
--- hs.hotkey.bind({ "cmd", "shift" }, "return", function()
---     hs.task
---         .new("/bin/zsh", nil, {
---             "-lc",
---             "open -n -a Kitty",
---         })
---         :start()
--- end)
+
 
 --------------------------------------------------
 -- App shortcuts
 --------------------------------------------------
 
--- Firefox (⌥ + F)
-hs.hotkey.bind({ "alt" }, "f", function()
+-- Firefox (⌥ + B)
+hs.hotkey.bind({ "alt" }, "B", function()
     launchApp("Firefox")
 end)
 
--- VS Code (⌥ + C)
-hs.hotkey.bind({ "alt" }, "c", function()
+-- VS Code (⌥ + V)
+hs.hotkey.bind({ "alt" }, "v", function()
     launchApp("Visual Studio Code")
 end)
 
@@ -207,3 +119,10 @@ end)
 hs.hotkey.bind({ "alt" }, "m", function()
     launchApp("Stremio", { fullscreen = true })
 end)
+
+hs.hotkey.bind({ "cmd", "shift" }, "return", function() launchKittyWithNvim() end)
+
+-- hs.hotkey.bind({ "cmd", "shift" }, "return",
+--     function()
+--         hs.task.new("/bin/zsh", nil, { "-lc", "open -n -a Kitty", }):start()
+--     end)
