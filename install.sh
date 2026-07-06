495f8efeb3f4b529eb91487de4829ce9cc08ba85
Enoch Abiodun <117421820+G00380316@users.noreply.github.com>
2026-07-07 00:44:01 +0100

    MINOR

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ packages=(
 	"bash-language-server" "texlab" "harper" "jdtls" "markdown-oxide"
 	"pyrefly" "quick-lint-js" "ruff" "rust-analyzer" "sqruff"
 	"superhtml" "llvm" "ty" "tinymist" "docker-language-server" "localsend"
-    "wgcf" "loop" "codex" "anomalyco/tap/opencode" "git-filter-repo"
+    "wgcf" "loop" "codex" "anomalyco/tap/opencode" "bfg"
 )
 
 npm_packages=(
